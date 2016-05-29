fec8dc42adae282f5f9f3ea0da7b2e67106e22dd
lastlocal <raj.k@lastlocal.in>
2016-05-29 16:11:26 +0530

    Rk local with MVC

---
+++ Bankable Birthday/View/Registration/Sign In/SignIn_VC.h
@@ -0,0 +1,13 @@
+//
+//  SignIn_VC.h
+//  Bankable Birthday
+//
+//  Created by Raj on 29/05/16.
+//  Copyright © 2016 Raj Kadam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignIn_VC : UIViewController
+
+@end
